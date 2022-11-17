2386acbbc72c262b3c54f5a12c92c10cd89d0656
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-11-16 21:20:14 -0300

    build: [backend] Install initial dependencies

---
+++ backend/src/database/config/config.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { Options } from 'sequelize';
+
+const config: Options = {
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || '',
+  database: process.env.POSTGRES_DB_NAME || 'db-bank',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: Number(process.env.POSTGRES_PORT) || 5432,
+  dialect: 'postgres',
+}
+
+export = config;
---
+++ backend/src/index.ts
@@ -0,0 +1,9 @@
+
+import 'dotenv/config';
+import app from './app';
+
+const PORT = process.env.APP_PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
